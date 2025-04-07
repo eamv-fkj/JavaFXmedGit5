4b46c02b2f92a1d9d308627c2f27e869aa917315
Flemming K. Jensen <flemming.k.jensen@gmail.com>
2025-04-07 20:36:13 +0200

    Initial commit
    Kun ændret .gitignore

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example._20251__javafx_med_git_5 {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens org.example._20251__javafx_med_git_5 to javafx.fxml;
+  exports org.example._20251__javafx_med_git_5;
+}
